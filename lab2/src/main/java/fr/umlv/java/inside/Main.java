80312080ddc151ef832909a09ab02270f835a313
Nicolas fau <nicolasfau93@gmail.com>
2019-10-01 10:19:51 +0200

    [UPDATE] toJSon MAIN

--- lab2/src/main/java/fr/umlv/java/inside/Main.java
+++ lab2/src/main/java/fr/umlv/java/inside/Main.java
@@ -1,6 +1,9 @@
 package fr.umlv.java.inside;
 
 import static java.util.stream.Collectors.joining;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class Main {
@@ -9,28 +12,36 @@ public class Main {
 		return Character.toLowerCase(name.charAt(3)) + name.substring(4);
 	}
 
+	private static String fieldAndFieldValue(Method e, Object obj, Object[] attr){
+			try {
+				return propertyName(e.getName()) + ":" + e.invoke(obj, attr);
+			} catch (IllegalAccessException | InvocationTargetException e1) {
+				return "";
+			}
+	}
+
 	public static String toJSON(Object obj) {
 		var listOfMethods = obj.getClass().getMethods();
 		return Arrays.stream(listOfMethods)
-			.map(e -> e.getName())
-			.filter(e -> e.substring(0, 3).equals("get"))
-			.map(Main::propertyName)
-			.collect(joining(",","{","}"));
-	}
-	/*public static String toJSON(Person person) {
-		return "{\n" + "  \"firstName\": \"" + person.getFirstName() + "\"\n" + "  \"lastName\": \""
-				+ person.getLastName() + "\"\n" + "}\n";
+				.filter(e -> e.getName().startsWith("get"))
+				.map(e -> fieldAndFieldValue(e, obj, new Object[0]))
+				.collect(joining(",\n\t", "{\n\t", "\n}"));
 	}
 
-	public static String toJSON(Alien alien) {
-		return "{\n" + "  \"planet\": \"" + alien.getPlanet() + "\"\n" + "  \"members\": \"" + alien.getAge() + "\"\n"
-				+ "}\n";
-	}*/
+	/*
+	 * public static String toJSON(Person person) { return "{\n" +
+	 * "  \"firstName\": \"" + person.getFirstName() + "\"\n" + "  \"lastName\": \""
+	 * + person.getLastName() + "\"\n" + "}\n"; }
+	 * 
+	 * public static String toJSON(Alien alien) { return "{\n" + "  \"planet\": \""
+	 * + alien.getPlanet() + "\"\n" + "  \"members\": \"" + alien.getAge() + "\"\n"
+	 * + "}\n"; }
+	 */
 
 	public static void main(String[] args) {
 		var person = new Person("John", "Doe");
 		System.out.println(toJSON(person));
-		//var alien = new Alien("E.T.", 100);
-		//System.out.println(toJSON(alien));
+		var alien = new Alien("E.T.", 100);
+		System.out.println(toJSON(alien));
 	}
 }
