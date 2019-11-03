b0216eb54e72e8ea07384864f086ae04b420f159
Nicolas FAU <nicolasfau93@gmail.com>
2019-11-03 13:04:52 +0100

    Create script_travis.sh

---
+++ script_travis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e # exit if return != 0
+if [ "$TARGET" = "lab6" ]; then
+    # download jdk-14
+    wget https://github.com/forax/java-next/releases/download/untagged-c59655314c1759142c15/jdk-14-loom-linux.tar.gz
+    # extract
+    tar xvf jdk-14-loom-linux.tar.gz
+    # export new environment variable JAVA_HOME
+    export JAVA_HOME=$(pwd)/jdk-14-loom/
+fi
+
+# for all lab
+cd $TARGET
+mvn package
